13cd7c2f3db26cbbf0b6fa255bef2eded56de9b0
Aoang <aoang@x2oe.com>
2025-07-21 19:42:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# @liwo/eslint-config
+
+来自 `@antfu/eslint-config` v4.17.0 的 Hard Fork
+
+它会更为偏激，且不适用于多数人。
+
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { antfu } from './factory'
+
+export * from './configs'
+export * from './factory'
+export * from './globs'
+export * from './types'
+export * from './utils'
+
+export default antfu
+
+// TODO add mutliple mode support
